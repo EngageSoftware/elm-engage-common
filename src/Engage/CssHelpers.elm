e4bf3ff95d7caaf022154ca90e3fd0d599ce9b0e
Jon Bockhorst <jbockhorst@engagesoftware.com>
2020-05-20 09:29:45 -0500

    Expose CssHelpers

--- src/Engage/CssHelpers.elm
+++ src/Engage/CssHelpers.elm
@@ -1,9 +1,17 @@
 module Engage.CssHelpers exposing (withNamespace)
 
+{-| CssHelpers
+
+@docs withNamespace
+
+-}
+
 import Html exposing (Attribute)
 import Html.Attributes
 
 
+{-| Get a class function that appends a namespace
+-}
 withNamespace : String -> (List String -> Attribute msg)
 withNamespace name =
     namespacedClass name
